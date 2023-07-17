fd92c80b4e44c60dc32d767d449d990915470359
MoekoOkada <moko.13.ts@gmail.com>
2023-07-17 15:26:50 +0200

    correction: typos, added: Advanced excercised

--- scripts/4_count.sh
+++ scripts/4_count.sh
@@ -13,7 +13,7 @@ for i in `ls *.eaglerc1.ref.bam`; do
   echo "# start ${f} " $(date)
 
   featureCounts -T 2 -t exon -g transcript_id -a ../genome/Ahal_v2_2.gtf -o ../4_count/${f}_hal_counts.txt ${i}
-  featureCounts -T 2 -t exon -g transcript_id -a ../genome/Ahal_v2_2.gtf -o ../4_count/${f}_hal_counts.txt ${f}.eaglerc1.alt.bam
+  featureCounts -T 2 -t exon -g transcript_id -a ../genome/Ahal_v2_2.gtf -o ../4_count/${f}_lyr_counts.txt ${f}.eaglerc1.alt.bam
 
   echo "# done ${f} " $(date)
 done
